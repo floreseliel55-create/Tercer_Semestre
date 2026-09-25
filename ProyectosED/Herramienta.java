f875c19d10f6b84d0f4f53cd30f19b9187bc4791
eliel <floreseliel55@gmail.com>
2026-09-25 16:09:47 -0700

    metodo devolver implementado

--- ProyectosED/Herramienta.java
+++ ProyectosED/Herramienta.java
@@ -18,13 +18,26 @@ public class Herramienta {
     public boolean prestar(int cantidad){
         int disponible=cantidadTotal-prestado;
         // comprueba si la cantidad a prestar sea mayor que 0, y si se ah prestado mas de la herramienta disponible y si se puede prestar la cantidad que me piden
-        if(cantidadTotal>0 && prestado<cantidadTotal && cantidad<=disponible){
-            
+        if(cantidadTotal>0 && prestado<cantidadTotal && cantidad<=disponible && cantidad>0){
+
             // se guarda que se ah prestado la herramienta
             prestado=prestado+cantidad;
             return true;
         }
         // de otro modo no se puede prestar
+        else{
+            return false;
+        }
+    }
+    // metodo que pide devolver una cantidad
+    public boolean devolver(int cantidad){
+        int disponible=cantidadTotal-prestado;
+        // esto comprueba que no se devuelva mas herramientas de las que hay en el taller y que no se puedan devolver negativos ni 0
+        if((cantidad+disponible)<=cantidadTotal && cantidad>0){
+            // al ser devueltas disminuye las que estan prestadas
+            prestado=prestado-cantidad;
+            return true;
+        }
         else{
             return false;
         }
